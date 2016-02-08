3d3b92a9b8b6ae167247de6bcb66499f7191b173
Kiryl_Sakalouski <kiryl_sakalouski@epam.com>
2016-02-08 13:26:49 +0300

    Added implementation of Firefox WebDriverConfig

--- QA.TestLibs.WebDriver/WebDriverConfig.cs
+++ QA.TestLibs.WebDriver/WebDriverConfig.cs
@@ -3,16 +3,17 @@
     using System;
     using QA.TestLibs.XmlType;
     using OpenQA.Selenium;
+    
     [XmlType("WebDriver configuration")]
-    public class WebDriverConfig
+    public abstract class WebDriverConfig
     {
         [XmlProperty("WebDriver type: Firefox, Chrome or IE")]
         [XmlLocation(XmlLocationType.Element | XmlLocationType.Attribute, "driverType", "webDriverType", "browser", "browserType")]
         public WebDriverType DriverType { get; set; }
 
         [XmlProperty("Is JavaScript enabled?")]
-        [XmlLocation(XmlLocationType.Element | XmlLocationType.Attribute, "isJavaScriptEnabled", "isJavaScript", "javaScriptEnabled")]
-        public bool JavaScriptEnabled { get; set; }
+        [XmlLocation(XmlLocationType.Element | XmlLocationType.Attribute, "isJavaScriptEnabled", "isJavaScript", "javaScript")]
+        public bool IsJavaScriptEnabled { get; set; }
 
         [XmlProperty("Timeout for element searching. Format: hh:mm:ss.ms")]
         [XmlLocation(XmlLocationType.Element | XmlLocationType.Attribute, "searchTimeout", "elementSearchTimeout", "implicitlyWait")]
@@ -21,6 +22,15 @@
         [XmlProperty("Timeout for page loading. Format: hh:mm:ss.ms")]
         [XmlLocation(XmlLocationType.Element | XmlLocationType.Attribute, "pageLoadTimeout", "pageTimeout")]
         public TimeSpan PageLoadTimeout { get; set; }
+
+        [XmlProperty("Timeout for executing javascript. Format: hh:mm:ss.ms")]
+        [XmlLocation(XmlLocationType.Element | XmlLocationType.Attribute, "javaScriptTimeout", "javascriptTimeout")]
+        [XmlConstraint("IsJavaScriptEnabled", true)]
+        public TimeSpan JavaScriptTimeout { get; set; }
+
+        [XmlProperty("Timeout for wait operations. Format: hh:mm:ss.ms")]
+        [XmlLocation(XmlLocationType.Element | XmlLocationType.Attribute, "waitTimeout")]
+        public TimeSpan WaitTimeout { get; set; }
 
         [XmlProperty("Is grid used?")]
         [XmlLocation(XmlLocationType.Element | XmlLocationType.Attribute, "isGrid", "isGridUsed")]
@@ -31,9 +41,23 @@
         [XmlConstraint("IsGrid", true)]
         public bool GridUri { get; set; }
 
-        public virtual IWebDriver CreateDriver()
+        public IWebDriver CreateDriver()
         {
-            return null;
+            IWebDriver driver = IsGrid
+                ? CreateRemoteDriver()
+                : CreateLocalDriver();
+
+            driver.Manage().Timeouts().SetPageLoadTimeout(PageLoadTimeout);
+            driver.Manage().Timeouts().ImplicitlyWait(SearchTimeout);
+
+            if (IsJavaScriptEnabled)
+                driver.Manage().Timeouts().SetScriptTimeout(JavaScriptTimeout);
+
+            return driver;
         }
+
+        public abstract IWebDriver CreateRemoteDriver();
+
+        public abstract IWebDriver CreateLocalDriver();
     }
 }
