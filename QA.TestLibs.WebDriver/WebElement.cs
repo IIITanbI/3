641de4da27ab73953f0fdcce61860c2653d3f447
Kiryl_Sakalouski <kiryl_sakalouski@epam.com>
2016-02-08 17:43:13 +0300

    sync

--- QA.TestLibs.WebDriver/WebElement.cs
+++ QA.TestLibs.WebDriver/WebElement.cs
@@ -6,10 +6,11 @@
     using System.Linq;
     using OpenQA.Selenium;
     using XmlType;
+    using System.Drawing;
 
     [XmlType("WebElement config")]
     [XmlLocation("webElement")]
-    public class WebElement : XmlBaseType
+    public class WebElement : XmlBaseType, IWebElement
     {
         [XmlProperty("List of child WebElements", IsAssignableTypesAllowed = true, IsRequired = false)]
         public List<WebElement> ChildWebElements { get; set; } = new List<WebElement>();
@@ -24,5 +25,101 @@
         [XmlProperty("Description of WebElement")]
         [XmlLocation(XmlLocationType.Element | XmlLocationType.Attribute, "elementDescription", "webElementDescription")]
         public string Description { get; set; }
+
+        public string TagName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool Enabled
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool Selected
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Point Location
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Size Size
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool Displayed
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendKeys(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Submit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Click()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAttribute(string attributeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCssValue(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IWebElement FindElement(By by)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReadOnlyCollection<IWebElement> FindElements(By by)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
