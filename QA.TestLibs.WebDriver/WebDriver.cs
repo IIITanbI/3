94d334b47d63ebbae0f237b6c1c7df0b478d2a5f
rosolko <rosolko@gmail.com>
2016-02-08 11:00:12 +0300

    Initial webdriver\webelement commit

--- QA.TestLibs.WebDriver/WebDriver.cs
+++ QA.TestLibs.WebDriver/WebDriver.cs
@@ -1,12 +1,32 @@
-﻿namespace QA.TestLibs.WebDriver
+﻿using OpenQA.Selenium;
+using System.Drawing;
+
+namespace QA.TestLibs.WebDriver
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    
-    public class WebDriver
+    public class WebDriver /*: IWebDriver*/
     {
+        private IWebDriver _driver;
+
+        public WebDriver() { }
+
+        public void WindowMaximize()
+        {
+            _driver.Manage().Window.Maximize();
+        }
+
+        public void SetWindowSize(int width, int height)
+        {
+            _driver.Manage().Window.Size = new Size(width, height);
+        }
+
+        public void SetWindowSize(Size size)
+        {
+            _driver.Manage().Window.Size = size;
+        }
+        public void TakeScreenshot(string screen)
+        {
+            Screenshot ss = ((ITakesScreenshot)_driver).GetScreenshot();
+            ss.SaveAsFile(screen, System.Drawing.Imaging.ImageFormat.Png);
+        }
     }
 }
---
+++ QA.TestLibs.WebDriver/WebElement.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using OpenQA.Selenium;
+
+namespace QA.TestLibs.WebDriver
+{
+    public class WebElement : IWebElement
+    {
+        public bool Displayed
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool Enabled
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Point Location
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool Selected
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Size Size
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string TagName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Click()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IWebElement FindElement(By by)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReadOnlyCollection<IWebElement> FindElements(By by)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAttribute(string attributeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCssValue(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendKeys(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Submit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
