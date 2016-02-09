6721bc2d212bf33e3cdb9a20012611ca2b76530d
Aliaksandr_Rasolka <aliaksandr_rasolka@epam.com>
2016-02-09 12:11:13 +0300

    Extend WebDriverManager.cs

--- QA.TestLibs.WebDriver/WebDriverManager.cs
+++ QA.TestLibs.WebDriver/WebDriverManager.cs
@@ -6,7 +6,9 @@
     using System.Diagnostics;
     using System.Threading;
     using OpenQA.Selenium.Support.UI;
-
+    using System.Drawing;
+    using System.Linq;
+    using OpenQA.Selenium.Interactions;
     [CommandManager("WebCommand",
         Description = "Manager for WebCommands")]
     public class WebDriverManager
@@ -28,26 +30,14 @@
             });
         }
 
-        public void HightLightElement(WebDriver driver, WebElement element, ILogger log)
-        {
-            string highlightJavascript = @"arguments[0].style.cssText = ""border-width: 2px; border-style: solid; border-color: red"";";
-            _container.Value._javaScriptExecutor.JSExecutor(highlightJavascript, new object[] { element });
-        }
-
-        public void UnHightLightElement(WebDriver driver, WebElement element, ILogger log)
-        {
-            string highlightJavascript = @"arguments[0].style.cssText = ""border-width: 0px; border-style: solid; border-color: red"";";
-            _container.Value._javaScriptExecutor.JSExecutor(highlightJavascript, new object[] { element });
-        }
-
-        public IWebElement Find(WebDriver driver, WebElement element, ILogger log)
+        public IWebElement Find(WebElement element, ILogger log)
         {
             log?.DEBUG($"Start searching element: {element.Name}");
             log?.TRACE($"{element}");
             try
             {
                 _sw.Value.Reset();
-                var el = driver.FindElement(element.Locator.Get());
+                var el = _container.Value._driver.FindElement(element.Locator.Get());
                 _sw.Value.Stop();
                 log?.DEBUG($"Element: {element.Name} has been found. Time: {_sw.Value.ElapsedMilliseconds} ms");
                 return el;
@@ -59,14 +49,52 @@
             }
         }
 
+        [Command("Set window size by size", Description = "Set window size using parameter {size}")]
+        public void SetWindowSize(Size size)
+        {
+            _container.Value._driver.Manage().Window.Size = size;
+        }
+
+        [Command("Set window size by width and height", Description = "Set window size using parameter {width} {height}")]
+        public void SetWindowSize(int width, int height)
+        {
+            _container.Value._driver.Manage().Window.Size = new Size(width, height);
+        }
+
+        [Command("Maximize window")]
+        public void WindowMaximize()
+        {
+            _container.Value._driver.Manage().Window.Maximize();
+        }
+
+        [Command("Accept alert")]
+        public void AcceptAlert()
+        {
+            IAlert alert = _container.Value._driver.SwitchTo().Alert();
+            alert.Accept();
+        }
+
+        [Command("Dismiss alert")]
+        public void DismissAlert()
+        {
+            IAlert alert = _container.Value._driver.SwitchTo().Alert();
+            alert.Dismiss();
+        }
+
+        [Command("Switch to new tab")]
+        public void SwitchToNewTab()
+        {
+            _container.Value._driver.SwitchTo().Window(_container.Value._driver.WindowHandles.Last());
+        }
+
         [Command("Wait until element is visible")]
-        public void WaitUntilElementIsVisible(WebDriver driver, IWebElement element, ILogger log)
+        public void WaitUntilElementIsVisible(IWebElement element, ILogger log)
         {
             log?.DEBUG("Wait until element is visible");
             try
             {
                 _sw.Value.Reset();
-                driver.WaitUntilElementIsVisible(element);
+                WaitUntilElementIsVisible(element);
                 _sw.Value.Stop();
                 log?.DEBUG($"Waiting for visibility has been completed. Time: {_sw.Value.ElapsedMilliseconds} ms");
             }
@@ -78,30 +106,51 @@
         }
 
         [Command("Wait until element is enabled")]
-        public void WaitUntilElementIsEnabled(WebDriver driver, IWebElement element, ILogger log)
+        public void WaitUntilElementIsEnabled(IWebElement element, ILogger log)
         {
             log?.DEBUG("Wait until element is enabled");
             try
             {
                 _sw.Value.Reset();
-                driver.WaitUntilElementIsVisible(element);
+                WaitUntilElementIsVisible(element);
                 _sw.Value.Stop();
-                log?.DEBUG($"Waitig for enabling has been completed. Time: {_sw.Value.ElapsedMilliseconds} ms");
+                log?.DEBUG($"Waiting for enabling has been completed. Time: {_sw.Value.ElapsedMilliseconds} ms");
             }
             catch (Exception ex)
             {
-                log?.ERROR("Waitig for enabling has been completed with exception");
-                throw new CommandAbortException("Waitig for enabling has been completed with exception", ex);
+                log?.ERROR("Waiting for enabling has been completed with exception");
+                throw new CommandAbortException("Waiting for enabling has been completed with exception", ex);
+            }
+        }
+
+        [Command("Move to", Description = "Move to element")]
+        public void MoveTo(WebElement element, ILogger log)
+        {
+            var el = Find(element, log);
+
+            WaitUntilElementIsVisible(el, log);
+            WaitUntilElementIsEnabled(el, log);
+
+            try
+            {
+                log?.INFO($"Move to element: {element.Name}");
+                new Actions(_container.Value._driver).MoveToElement(el).Build().Perform();
+                log?.INFO("Move to completed");
+            }
+            catch (Exception ex)
+            {
+                log?.ERROR($"Error occurred moving to element: {element.Name}");
+                throw new CommandAbortException($"Error occurred moving to element: {element.Name}", ex);
             }
         }
 
         [Command("Click", Description = "Click to element")]
-        public void Click(WebDriver driver, WebElement element, IContext contex, ILogger log)
+        public void Click(WebElement element, ILogger log)
         {
-            var el = Find(driver, element, log);
+            var el = Find(element, log);
 
-            WaitUntilElementIsVisible(driver, el, log);
-            WaitUntilElementIsEnabled(driver, el, log);
+            WaitUntilElementIsVisible(el, log);
+            WaitUntilElementIsEnabled(el, log);
 
             try
             {
@@ -111,9 +160,100 @@
             }
             catch (Exception ex)
             {
-                log?.ERROR($"Error occured during clicking on element: {element.Name}");
-                throw new CommandAbortException($"Error occured during clicking on element: {element.Name}", ex);
+                log?.ERROR($"Error occurred during clicking on element: {element.Name}");
+                throw new CommandAbortException($"Error occurred during clicking on element: {element.Name}", ex);
             }
+        }
+
+        [Command("Right click", Description = "Right click to element")]
+        public void RightClick(WebElement element, ILogger log)
+        {
+            var el = Find(element, log);
+
+            WaitUntilElementIsVisible(el, log);
+            WaitUntilElementIsEnabled(el, log);
+
+            try
+            {
+                log?.INFO($"Right click on element: {element.Name}");
+                new Actions(_container.Value._driver).MoveToElement(el).ContextClick().Build().Perform();
+                log?.INFO("Right click completed");
+            }
+            catch (Exception ex)
+            {
+                log?.ERROR($"Error occurred during right-clicking on element: {element.Name}");
+                throw new CommandAbortException($"Error occurred during right-clicking on element: {element.Name}", ex);
+            }
+        }
+
+        [Command("Double click", Description = "Double click to element")]
+        public void DoubleClick(WebElement element, ILogger log)
+        {
+            var el = Find(element, log);
+
+            WaitUntilElementIsVisible(el, log);
+            WaitUntilElementIsEnabled(el, log);
+
+            try
+            {
+                log?.INFO($"Double click on element: {element.Name}");
+                new Actions(_container.Value._driver).MoveToElement(el).DoubleClick().Build().Perform();
+                log?.INFO("Double click completed");
+            }
+            catch (Exception ex)
+            {
+                log?.ERROR($"Error occurred during double-clicking on element: {element.Name}");
+                throw new CommandAbortException($"Error occurred during-clicking on element: {element.Name}", ex);
+            }
+        }
+
+        public void WaitForPageToLoad()
+        {
+            _container.Value._wait.Until(d => Equals(_container.Value._javaScriptExecutor.ObjectJSExecutor("return document.readyState").ToString().ToLower(), "complete"));
+        }
+
+        public Screenshot TakeScreenshot(ILogger log)
+        {
+            _container.Value._driver.SwitchTo().Window(_container.Value._driver.CurrentWindowHandle);
+            Screenshot screenshot = ((ITakesScreenshot)_container.Value._driver).GetScreenshot();
+            return screenshot;
+        }
+
+        public void SaveScreenshot(Screenshot screenshot, string path)
+        {
+            screenshot.SaveAsFile(path, System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        public void HightLightElement(IWebElement element)
+        {
+            string highlightJavascript = @"arguments[0].style.cssText = ""border-width: 2px; border-style: solid; border-color: red"";";
+            _container.Value._javaScriptExecutor.JSExecutor(highlightJavascript, new object[] { element });
+        }
+
+        public void UnHightLightElement(IWebElement element)
+        {
+            string highlightJavascript = @"arguments[0].style.cssText = ""border-width: 0px; border-style: solid; border-color: red"";";
+            _container.Value._javaScriptExecutor.JSExecutor(highlightJavascript, new object[] { element });
+        }
+
+        public void WaitUntilElementIsVisible(By by)
+        {
+            _container.Value._wait.Until(driver => driver.FindElement(by)?.Displayed);
+        }
+
+        public void WaitUntilElementIsVisible(IWebElement element)
+        {
+            _container.Value._wait.Until(driver => element.Displayed);
+        }
+
+        public void WaitUntilElementIsEnabled(By by)
+        {
+            _container.Value._wait.Until(driver => driver.FindElement(by)?.Enabled);
+        }
+
+        public void WaitUntilElementIsEnabled(IWebElement element)
+        {
+            _container.Value._wait.Until(driver => element.Enabled);
         }
 
         private class LocalContainer
