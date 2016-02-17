2c81b968dce7a89a56cac0580f1430ce9660c159
Aliaksandr_Rasolka <aliaksandr_rasolka@epam.com>
2016-02-17 15:01:07 +0300

    generator js fix

--- QA.TestLibs.Report/ReportGenerator.cs
+++ QA.TestLibs.Report/ReportGenerator.cs
@@ -16,6 +16,7 @@
             report.AddFirst(new XDocumentType("html", null, null, null));
 
             var html = new XElement("html");
+            html.Add(new XAttribute("lang", "en"));
             html.Add(GetHead());
             html.Add(GetBody(testItem, testEnvironmentInfo));
 
@@ -55,15 +56,19 @@
         {
             var body = new XElement("body");
 
-            var js = new XElement("script");
+            var js = new XElement("script", "");
             js.Add(new XAttribute("src", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"));
             js.Add(new XAttribute("integrity", "sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"));
             js.Add(new XAttribute("crossorigin", "anonymous"));
 
+            var jQuery = new XElement("script", "");
+            jQuery.Add(new XAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"));
+
             var container = new XElement("div", new XAttribute("class", "container"));
             container.Add(GetEnvironment(testEnvironmentInfo));
+            container.Add(GetReport(testItem));
 
-            body.Add(container, js);
+            body.Add(container, jQuery, js);
 
             return body;
         }
@@ -73,7 +78,7 @@
             var environment = new XElement("div");
             environment.Add(new XAttribute("class", "panel panel-default"));
 
-            var heading = new XElement("div", "Environment");
+            var heading = new XElement("div", new XElement("mark", "Environment"));
             heading.Add(new XAttribute("class", "panel-heading"));
 
             var table = new XElement("table");
@@ -92,19 +97,81 @@
 
             var tbody = new XElement("tbody");
             var trb = new XElement("tr");
-            trb.Add(new XElement("th", testEnvironmentInfo.CLRVersion));
-            trb.Add(new XElement("th", testEnvironmentInfo.OSName));
-            trb.Add(new XElement("th", testEnvironmentInfo.OSVersion));
-            trb.Add(new XElement("th", testEnvironmentInfo.Platform));
-            trb.Add(new XElement("th", testEnvironmentInfo.MachineName));
-            trb.Add(new XElement("th", testEnvironmentInfo.User));
-            trb.Add(new XElement("th", testEnvironmentInfo.UserDomain));
+            trb.Add(new XElement("td", testEnvironmentInfo.CLRVersion));
+            trb.Add(new XElement("td", testEnvironmentInfo.OSName));
+            trb.Add(new XElement("td", testEnvironmentInfo.OSVersion));
+            trb.Add(new XElement("td", testEnvironmentInfo.Platform));
+            trb.Add(new XElement("td", testEnvironmentInfo.MachineName));
+            trb.Add(new XElement("td", testEnvironmentInfo.User));
+            trb.Add(new XElement("td", testEnvironmentInfo.UserDomain));
             tbody.Add(trb);
 
             table.Add(thead, tbody);
 
             environment.Add(heading, table);
             return environment;
+        }
+
+        public XElement GetReport(TestItem testItem)
+        {
+            var report = new XElement("div",
+                new XAttribute("class", "panel-group"),
+                new XAttribute("role", "tablist")
+            );
+
+            var panel = new XElement("div", new XAttribute("class", "panel panel-default"));
+
+            var panelHead = new XElement("div",
+                new XAttribute("class", "panel-heading"),
+                new XAttribute("role", "tab"),
+                new XAttribute("id", "collapseListGroupHeading1")
+            );
+
+            var panelHeadLink = new XElement("h4",
+                new XAttribute("class", "panel-title"),
+                new XElement("a",
+                    new XAttribute("class", "collapsed"),
+                    new XAttribute("role", "button"),
+                    new XAttribute("data-toggle", "collapse"),
+                    new XAttribute("href", "#collapseListGroup1"),
+                    new XAttribute("aria-expanded", "false"),
+                    new XAttribute("aria-controls", "collapseListGroup1"),
+                    $"Name: {testItem.Name}, Description: {testItem.Description}"
+            ));
+
+            panelHead.Add(panelHeadLink);
+            panel.Add(panelHead);
+
+            if (testItem.Childs.Count != 0)
+            {
+                var panelGroup = new XElement("div",
+                    new XAttribute("id", "collapseListGroup1"),
+                    new XAttribute("class", "panel-collapse collapse"),
+                    new XAttribute("role", "tabpanel"),
+                    new XAttribute("aria-labelledby", "collapseListGroupHeading1"),
+                    new XAttribute("aria-expanded", "false")
+                );
+
+                var ul = new XElement("ul",
+                    new XAttribute("class", "list-group")
+                );
+
+                foreach (var item in testItem.Childs)
+                {
+                    ul.Add(new XElement("li",
+                        new XAttribute("class", "list-group-item"),
+                        $"Name: {item.Name}, Description: {item.Description}"
+                    ));
+                }
+
+                panelGroup.Add(ul);
+
+                panel.Add(panelGroup);
+            }
+
+            report.Add(panel);
+
+            return report;
         }
     }
 }
