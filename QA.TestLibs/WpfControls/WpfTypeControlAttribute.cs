04c9b2e83d6c72add5e2eaae3785e20adc8454a0
Kiryl_Sakalouski <kiryl_sakalouski@epam.com>
2016-02-19 13:22:45 +0300

    Removed annecessary Project Added attribute for XmlTypeControl

---
+++ QA.TestLibs/WpfControls/WpfTypeControlAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace QA.TestLibs.WpfControls
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public class WpfTypeControlAttribute : Attribute
+    {
+        public Type XmlType { get; private set; }
+
+        public WpfTypeControlAttribute(Type xmlType)
+        {
+            XmlType = xmlType;
+        }
+    }
+}
