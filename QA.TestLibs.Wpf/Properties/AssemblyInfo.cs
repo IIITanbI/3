073e0702f713113f7fae6917ad1c96e9a66c211d
Kiryl_Sakalouski <kiryl_sakalouski@epam.com>
2016-02-19 12:37:27 +0300

    Added Base projects for Wpf Controls Removed Test projects for WpfControls

--- QA.TestLibs.Wpf/Properties/AssemblyInfo.cs
+++ QA.TestLibs.Wpf/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QA.TestLibs.Wpf.XmlTypeControl")]
+[assembly: AssemblyTitle("QA.TestLibs.Wpf")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EPAM Systems")]
-[assembly: AssemblyProduct("QA.TestLibs.Wpf.XmlTypeControl")]
+[assembly: AssemblyProduct("QA.TestLibs.Wpf")]
 [assembly: AssemblyCopyright("Copyright © EPAM Systems 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
