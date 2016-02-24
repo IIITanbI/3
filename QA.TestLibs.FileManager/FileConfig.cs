f7dd67648eb2b6c9013938937895e20afcabe389
Aliaksandr_Rasolka <aliaksandr_rasolka@epam.com>
2016-02-24 15:47:54 +0300

    Update Tutorial manager. Initial file manager commit

---
+++ QA.TestLibs.FileManager/FileConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace QA.TestLibs.FileManager
+{
+    using QA.TestLibs.XmlDesiarilization;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [XmlType("File config")]
+    public class FileConfig : XmlBaseType
+    {
+    }
+}
---
+++ QA.TestLibs.FileManager/FileManager.cs
@@ -0,0 +1,14 @@
+﻿namespace QA.TestLibs.FileManager
+{
+    using QA.TestLibs.Commands;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [CommandManager(typeof(FileConfig), Description = "Manager for tutorial")]
+    public class FileManager
+    {
+    }
+}
