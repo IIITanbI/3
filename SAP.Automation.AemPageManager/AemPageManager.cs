8c46595bb42d61f4aae31a58dbe1133df665a400
Aliaksandr_Rasolka <aliaksandr_rasolka@epam.com>
2016-02-26 11:45:37 +0300

    Extend AemPageManager with GenerateCommandForActivatePage command

--- SAP.Automation.AemPageManager/AemPageManager.cs
+++ SAP.Automation.AemPageManager/AemPageManager.cs
@@ -35,5 +35,25 @@
                 throw new CommandAbortException($"Error occurred during generating command for aem page '{aemPage.Title}' creation", ex);
             }
         }
+
+        [Command("ActivatePageCmd", Description = "Generate command for aem page activation")]
+        public string GenerateCommandForActivatePage(AemPage aemPage, ILogger log)
+        {
+            try
+            {
+                log?.DEBUG($"Generate command for aem page '{aemPage.Title}' activation");
+
+                var tmp = $"?cmd=Activate&path={aemPage.ParentPath}/{aemPage.Title}";
+
+                log?.DEBUG($"Generating command for aem page '{aemPage.Title}' activation completed");
+
+                return tmp;
+            }
+            catch (Exception ex)
+            {
+                log?.ERROR($"Error occurred during generating command for aem page '{aemPage.Title}' activation");
+                throw new CommandAbortException($"Error occurred during generating command for aem page '{aemPage.Title}' activation", ex);
+            }
+        }
     }
 }
