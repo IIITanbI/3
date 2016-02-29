26599afc089234168223caff6f1d27b740f391e7
IIITanbI <paladinama@tut.by>
2016-02-29 11:06:46 +0300

    edit js

--- QA.TestLibs.Report/filter js1/logFilter.js
+++ QA.TestLibs.Report/filter js1/logFilter.js
@@ -4,15 +4,15 @@ $(function () {
     myFilter.multiSelect = false;
 
     myFilter.getChilds = function (button) {
-        return $(button).closest(".logPanel").children('.log').children("div");
+        return $(button).closest(".logPanel").children('.logs').children();
     };
 
     myFilter.getFilterButtons = function (button) {
-        return $(button).closest(".log-filter-btns").find("button[class*='log-filter']");
+        return $(button).closest(".log-fltr-btns").children();
     };
 
-    myFilter.getTotalButton = function (button) {
-        return $(button).closest(".log-filter-btns").find(".log-filter-trace.log-filter-debug.log-filter-warn.log-filter-info.log-filter-error");
+    myFilter.getDefaultButton = function (button) {
+        return $(button).closest(".log-fltr-btns").children().first();
     };
 
     myFilter.getChildStatus = function (child) {
@@ -20,41 +20,10 @@ $(function () {
         return $status;
     };
 
-    myFilter.getDeactivatedClass = function (button) {
-        var filter = myFilter.getFilterFromButton(button);
-        var str = filter.join(" ");
 
-        var res = "btn-";
-        switch (str) {
-            case "trace debug warn info error":
-                res += "primary";
-                break;
-            case "debug warn info error":
-                res += "success";
-                break;
-            case "warn info error":
-                res += "warning";
-                break;
-            case "info error":
-                res += "info";
-                break;
-            case "error":
-                res += "danger";
-                break;
-            default:
-                res += "info";
-                break;
-        }
-        return res;
-    };
-
-
-
-    $(".log-filter-button-expander").children("span").attr("class", "glyphicon glyphicon-chevron-right");
-
-    $(".log-filter-button-expander").click(function (e) {
+    $(".log-fltr-btn-exp").click(function (e) {
         var $cur = $(e.currentTarget).children("span");
-        var $elem = $(this).closest(".logHeader").find(".log-filter-btns");
+        var $elem = $(this).closest(".logHeader").find(".log-fltr-btns");
         $elem.toggle(300, function onCompleteToggle() {
             if ($elem.is(":visible")) {
                 $cur.attr("class", "glyphicon glyphicon-chevron-left");
@@ -66,7 +35,14 @@ $(function () {
         });
     });
 
-    $("button[class*='log-filter']").click(function (e) {
+    myFilter.prepare($(".log-fltr-btns button").first());
+
+    var $btnsPanel = $(".log-fltr-btns");
+    for(var i = 0; i < $btnsPanel.length; i++){
+        myFilter.prepare($($btnsPanel[i]).find("button").first());
+    }
+
+    $(".log-fltr-btns button").click(function (e) {
         myFilter.filterButtonClick(this);
     });
 });
